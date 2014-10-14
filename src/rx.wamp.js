49fbeeb1feba3286936148f93fcde21f0fdfc152
paulpdaniels <paulpdaniels@gmail.com>
2014-10-14 00:31:21 -0700

    Wrapped definition.  Cleaned out old files

--- src/rx.wamp.js
+++ src/rx.wamp.js
@@ -1,127 +1,141 @@
-var autobahn = require('autobahn');
-var Rx = require('rx');
+'use strict';
 
-var Observable = Rx.Observable;
+var _autobahn = undefined;
+var _rx = undefined;
+
+try {
+    _autobahn = require('autobahn');
+    _rx = require('rx');
+} catch (e) {
+    //These are defined by the browser
+    _autobahn = autobahn;
+    _rx = Rx;
+}
+
+(function(Rx, autobahn) {
+
+    var Observable = Rx.Observable;
 
 
-autobahn.connectObservable = function (opts, reconnect) {
+    autobahn.connectObservable = function (opts, reconnect) {
 
-    return Observable.createWithDisposable(function (observer) {
+        return Observable.createWithDisposable(function (observer) {
 
-        var connection = new autobahn.Connection(opts);
-        var shouldReconnect = new Rx.BehaviorSubject(true);
+            var connection = new autobahn.Connection(opts);
+            var shouldReconnect = new Rx.BehaviorSubject(true);
 
-        (reconnect || Observable.empty()).subscribe(shouldReconnect);
+            (reconnect || Observable.empty()).subscribe(shouldReconnect);
 
-        function onOpen(session) {
-            observer.onNext(session);
-        }
+            function onOpen(session) {
+                observer.onNext(session);
+            }
 
-        function onClose(reason, detail) {
+            function onClose(reason, detail) {
 
-            if (reason === 'closed') {
-                observer.onCompleted();
-            } else if (reason === 'unreachable') {
-                observer.onError({reason: reason, detail: detail});
-            } else if (reason === 'lost') {
-
-                if (!shouldReconnect.value) {
+                if (reason === 'closed') {
                     observer.onCompleted();
-                    return false;
-                } else {
-                    return true;
+                } else if (reason === 'unreachable') {
+                    observer.onError({reason: reason, detail: detail});
+                } else if (reason === 'lost') {
+
+                    if (!shouldReconnect.value) {
+                        observer.onCompleted();
+                        return false;
+                    } else {
+                        return true;
+                    }
                 }
             }
-        }
 
-        connection.onopen = onOpen;
-        connection.onclose = onClose;
+            connection.onopen = onOpen;
+            connection.onclose = onClose;
 
-        connection.open();
+            connection.open();
 
-        return function () {
-            if (connection.isOpen) {
-                connection.close();
+            return function () {
+                if (connection.isOpen) {
+                    connection.close();
+                }
+            };
+
+        }).publish().refCount();
+    };
+
+    var sessionProto = autobahn.Session.prototype;
+
+    sessionProto.subscribeObservable = function (topic, options) {
+
+        var self = this;
+
+        return Observable.create(function (observer) {
+
+            var subject = new Rx.Subject();
+
+            function onNext(args, kwargs, details) {
+                subject.onNext({args: args, kwargs: kwargs, details: details});
             }
-        };
 
-    }).publish().refCount();
-};
+            //Creates an observable from the promise
+            var subscriber = Observable.fromPromise(self.subscribe(topic, onNext, options));
 
-var sessionProto = autobahn.Session.prototype;
+            var unsubscriber = subscriber.flatMap(function (token) {
+                return Observable.fromPromise(self.unsubscribe(token));
+            }).ignoreElements();
 
-sessionProto.subscribeObservable = function (topic, options) {
+            //Forward the errors from connection on to the end user
+            var topicSubscription = subscriber
+                .map(function (_) {
+                    //Ignore the incoming event and just send the subject
+                    return subject.asObservable();
+                })
+                .subscribe(observer.onNext.bind(observer),
+                observer.onError.bind(observer));
 
-    var self = this;
+            var disposable = Rx.Disposable.create(function () {
+                topicSubscription.dispose();
+                //Automatically unsubscribe
+                unsubscriber.
+                    subscribe(function () {
+                    },
+                    observer.onError.bind(observer),
+                    observer.onCompleted.bind(observer));
+            });
 
-    return Observable.create(function (observer) {
 
-        var subject = new Rx.Subject();
+            return new Rx.RefCountDisposable(disposable);
+        });
+    };
 
-        function onNext(args, kwargs, details) {
-            subject.onNext({args: args, kwargs: kwargs, details: details});
-        }
+    sessionProto.registerObservable = function (procedure, endpoint, options) {
 
-        //Creates an observable from the promise
-        var subscriber = Observable.fromPromise(self.subscribe(topic, onNext, options));
+        var self = this;
 
-        var unsubscriber = subscriber.flatMap(function (token) {
-            return Observable.fromPromise(self.unsubscribe(token));
-        }).ignoreElements();
+        return Observable.create(function (observer) {
 
-        //Forward the errors from connection on to the end user
-        var topicSubscription = subscriber
-            .map(function (_) {
-                //Ignore the incoming event and just send the subject
-                return subject.asObservable();
-            })
-            .subscribe(observer.onNext.bind(observer),
-            observer.onError.bind(observer));
+            var subscriber = Observable.fromPromise(self.register(procedure, endpoint, options));
+            var unsubscriber = subscriber.flatMap(function (sub) {
+                return Observable.fromPromise(self.unregister(sub));
+            }).ignoreElements();
 
-        var disposable = Rx.Disposable.create(function(){
-            topicSubscription.dispose();
-            //Automatically unsubscribe
-            unsubscriber.
-                subscribe(function () {
-                },
-                observer.onError.bind(observer),
-                observer.onCompleted.bind(observer));
+            subscriber.subscribe(observer.onNext.bind(observer), observer.onError.bind(observer));
+
+            return function () {
+                unsubscriber.subscribe(observer);
+            }
         });
 
+    };
 
-        return new Rx.RefCountDisposable(disposable);
-    });
-};
+    sessionProto.callObservable = function (procedure, args, kwargs, options) {
+        return Observable.fromPromise(this.call(procedure, args, kwargs, options));
+    };
 
-sessionProto.registerObservable = function (procedure, endpoint, options) {
+    sessionProto.publishObservable = function (topic, args, kwargs, options) {
 
-    var self = this;
+        var self = this;
 
-    return Observable.create(function (observer) {
+        var publication = self.publish(topic, args, kwargs, options);
 
-        var subscriber = Observable.fromPromise(self.register(procedure, endpoint, options));
-        var unsubscriber = subscriber.flatMap(function (sub) {
-            return Observable.fromPromise(self.unregister(sub));
-        }).ignoreElements();
-
-        subscriber.subscribe(observer.onNext.bind(observer), observer.onError.bind(observer));
-
-        return function () {
-            unsubscriber.subscribe(observer);
-        }
-    });
-
-};
-
-sessionProto.callObservable = function (procedure, args, kwargs, options) {
-    return Observable.fromPromise(this.call(procedure, args, kwargs, options));
-};
-
-sessionProto.publishObservable = function (topic, args, kwargs, options) {
-
-    var self = this;
-
-    var publication = self.publish(topic, args, kwargs, options);
-
-    return publication !== undefined ? Observable.fromPromise(publication) : Observable.empty();
-};
+        return publication !== undefined ? Observable.fromPromise(publication) : Observable.empty();
+    };
+})(_rx, _autobahn);
--- test/subject/subjects.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Paul on 10/9/2014.
- */
